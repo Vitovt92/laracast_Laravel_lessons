89d6e2fbd8ce47847626d0abb891e2c4cdbf37b2
Vitovt92 <Sergeevvs92@gmail.com>
2021-11-11 16:35:29 +0200

    lesson 34

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,27 @@
 <x-layout>
-    @foreach ($posts as $post)
+
+    @include ("_posts-header")
+    @if ($posts->count())
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card :post="$posts[0]" />
+
+        @if ($posts->count() > 1)
+        <div class="lg:grid lg:grid-cols-6">
+
+                @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"/>
+
+                @endforeach
+        </div>
+        @endif
+    </main>
+
+    @else
+        No posts yet
+    @endif
+
+    <!-- @foreach ($posts as $post)
     <article>
 
         <h1><a href="/posts/{{$post->slug}}"> {{$post->title}} </a></h1>
@@ -10,6 +32,6 @@
             {{$post->excerpt}}
         </div>
     </article>
-    @endforeach
+    @endforeach -->
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -35,12 +36,15 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('category/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->post
+        'posts' => $category->post,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->post
+        'posts' => $author->post,
+        'categories' => Category::all()
     ]);
 });
