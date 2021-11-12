653a3624203d53214b13286e44abf80bba759288
Vitovt92 <Sergeevvs92@gmail.com>
2021-11-12 15:29:04 +0200

    lesson 35

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,14 @@
+@props(['trigger'])
+
+<div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
+
+            <div x-data="{ show: false}" @click.away="show = false">
+                <div @click="show = ! show">
+                    {{$trigger}}
+                </div>
+
+                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display: none">
+                     {{$slot}}
+                </div>
+            </div>
+</div>
