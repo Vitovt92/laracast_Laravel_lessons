2bcbc8e9cbf2013876fc6c358aa79581d63e122d
Vitovt92 <Sergeevvs92@gmail.com>
2021-11-15 17:18:29 +0200

    Lesson 45

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\User;
@@ -25,8 +26,7 @@ Route::get('/', [PostController::class, "index"])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, "show"]);
 
-// Route::get('authors/{author:username}', function (User $author) {
-//     return view('posts.index', [
-//         'posts' => $author->post,
-//     ]);
-// });
+Route::get('register', [RegisterController::class, "create"]);
+
+Route::post('register', [RegisterController::class, "store"]);
+
