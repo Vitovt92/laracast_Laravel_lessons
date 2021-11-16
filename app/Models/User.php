1993033c0a6fd7c5617a47199e2c8b9f9c3a2af1
Vitovt92 <Sergeevvs92@gmail.com>
2021-11-16 11:24:54 +0200

    Lesson 46

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,9 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
     /**
      * The attributes that should be cast.
      *
