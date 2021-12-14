3736313340a7c268bbb6b4dd0a1ac2df7c459f9a
Vitovt92 <Sergeevvs92@gmail.com>
2021-12-14 15:13:05 +0200

    Lesson 52

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,20 @@
+<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
+                        <div class="flex-shrink-0" >
+                            <img src="https://i.pravatar.cc/60" width="60" height="60" alt="avatar" class="rounded-xl">
+                        </div>
+                        <div>
+                            <header class="mb-4">
+                                <h3 class="font-bold">John Doe</h3>
+                                <p class="text-xs">Posted
+                                    <time>8 months ago</time>
+                                </p>
+                            </header>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui fuga incidunt
+                                explicabo illum quas illo consequuntur consequatur rem quidem sapiente libero
+                                rerum unde nam odit, earum necessitatibus vel reiciendis impedit ut? Totam
+                                distinctio repellendus, quaerat iusto iure a at voluptatum tempora, mollitia,
+                                odio ratione dignissimos porro hic. Ab, harum amet.
+                            </p>
+                        </div>
+                    </article>
